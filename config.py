10c5d2cf1c8b7303c720fc3699760c32b76aeab4
praveentn <sigmoidptn@gmail.com>
2026-04-26 06:47:44 +0530

    db persistence

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-DB_PATH = "wordle_bot.db"
+DB_PATH = os.path.join("data", "wordle_bot.db")
 MAX_ATTEMPTS = 600
 WORD_LENGTH = 5
 CHAIN_TIMEOUT = 600  # seconds per turn (informational only, not enforced by timer)
